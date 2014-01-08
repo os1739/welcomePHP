1627c449575f0dbf4cedcda762e4cc5afe68a8d2
ok <ok@okPC>
2014-01-08 16:58:49 +0900

    성공입니다요

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=MS949">
+<title>Insert title here</title>
+</head>
+
+<body>
+	<?php 
+		echo "성공이요";
+	
+	?>
+</body>
+</html>
